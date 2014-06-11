75754c659686901bb13f4efb0b1510aac13c094a
Greg Stewart <gstewart@microsoft.com>
2014-06-11 09:20:39 +0100

    Expanded the interface to to accept a Coordinate object and still be backward compatible

--- spec/parallel-change/GridSpec.js
+++ spec/parallel-change/GridSpec.js
@@ -1,4 +1,4 @@
-describe("Grid", function() {
+describe("Grid", function () {
     var grid;
 
     beforeEach(function () {
@@ -9,7 +9,7 @@ describe("Grid", function() {
         var x = 0,
             y = 0;
 
-        expect(grid.isEmpty(x,y)).toBe(true);
+        expect(grid.isEmpty(x, y)).toBe(true);
     });
 
     it('returns false if the grid has a value', function () {
@@ -17,9 +17,9 @@ describe("Grid", function() {
             x = 0,
             y = 0;
 
-        grid.addCell(x, y , cell);
+        grid.addCell(x, y, cell);
 
-        expect(grid.isEmpty(x,y)).toBe(false);
+        expect(grid.isEmpty(x, y)).toBe(false);
     });
 
     it('returns a cell from a given x/y co-ordinate', function () {
@@ -28,20 +28,41 @@ describe("Grid", function() {
             y = 0,
             storedCell;
 
-        grid.addCell(x, y , cell);
-        storedCell = grid.fetchCell(x,y);
+        grid.addCell(x, y, cell);
+        storedCell = grid.fetchCell(x, y);
 
         expect(storedCell).toBe(cell);
     });
 
     it('add a cell to the proper co-ordinates', function () {
-       var cell = {},
-           x = 0,
-           y = 0;
+        var cell = {},
+            x = 0,
+            y = 0;
 
-        grid.addCell(x, y , cell);
+        grid.addCell(x, y, cell);
 
-        expect(grid.fetchCell(x,y)).toBe(cell);
+        expect(grid.fetchCell(x, y)).toBe(cell);
     });
 
+    describe('expand interface', function () {
+        it('accepts a coordinate object and doesn\'t break the api', function () {
+            var x = 0,
+                y = 0,
+                Coordinates = {x: x, y: y},
+                cell = {};
+
+            grid.addCell(Coordinates, cell);
+
+            expect(grid.fetchCell(Coordinates)).toBe(cell);
+        });
+
+        it('should still return tru for an empty cell', function () {
+            var x = 0,
+                y = 0,
+                Coordinates = {x: x, y: y};
+
+            expect(grid.isEmpty(Coordinates)).toBe(true);
+        });
+
+    });
 });
--- src/parallel-change/Grid.js
+++ src/parallel-change/Grid.js
@@ -1,15 +1,38 @@
 function Grid() {
     this.cells = [[]];
+    this.newCells = [];
+
+    this.findCell = function (coordinate) {
+        var foundCell;
+        this.newCells.forEach(function (cell) {
+            if (cell.coordinate.x === coordinate.x && cell.coordinate.y === coordinate.y) {
+                foundCell = cell.cell;
+            }
+        });
+        return foundCell;
+    };
 }
 
 Grid.prototype.addCell = function (x, y, cell) {
-    this.cells[x][y] = cell;
+    if(arguments.length > 2 && !isNaN(x)) {
+        this.cells[x][y] = cell;
+    } else {
+        this.newCells.push({coordinate: x, cell: y});
+    }
 };
 
 Grid.prototype.fetchCell = function (x, y) {
-    return this.cells[x][y];
+    if(arguments.length > 1 && !isNaN(x)) {
+        return this.cells[x][y];
+    } else {
+        return this.findCell(x);
+    }
 };
 
 Grid.prototype.isEmpty = function (x, y) {
-    return this.cells[x][y] == null;
+    if(arguments.length > 1 && !isNaN(x)) {
+        return this.cells[x][y] == null;
+    } else {
+        return this.findCell(x) == null;
+    }
 };
